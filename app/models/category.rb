cea48a0ebe793220090d37dea8c850fcc1bb9ca3
NancyGoodman <Nancygoodman03@gmail.com>
2016-06-07 19:38:56 -0400

    removed validation from category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ActiveRecord::Base
   has_many :brand_categories
   has_many :brands, through: :brand_categories
 
-  validates :house, presence: true, uniqueness: true
+  # validates :house, presence: true, uniqueness: true
 end
