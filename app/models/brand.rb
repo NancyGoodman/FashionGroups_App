485a6366f69d29f15a57bc446b8dc5e836b5987d
NancyGoodman <Nancygoodman03@gmail.com>
2016-06-07 20:18:57 -0400

    removed validation house from brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -10,6 +10,5 @@ class Brand < ActiveRecord::Base
   has_attached_file :logo, :styles => { :thumb => "100x100>", :medium => "250x250>", :small => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 
-
-  validates :house, presence: true, uniqueness: {case_sensitive: false}
+  # validates :house, presence: true, uniqueness: {case_sensitive: false}
 end
